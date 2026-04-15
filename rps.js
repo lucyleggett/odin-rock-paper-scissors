71c0452486fa5afb745da6cabdef7a8ae857633e
lucyleggett <lucy.leggett@hotmail.com>
2026-04-15 16:42:19 +0800

    Create getHumanChoice function Includes the conditions for winning so that the result of the function is humanScore/computerScore += 1

--- rps.js
+++ rps.js
@@ -1,1 +1,44 @@
-console.log("Hello World")
+/*
+Create global variables humanScore and computerScore with initial value of 0.
+Create variables "Rock", "Paper" and "Scissors" and assign them the values 1, 2 and 3 respectively.
+Create function getComputerChoice to generate a random integer between 1 and 3.
+Create function getHumanChoice to receive input as "Rock", "Paper" or "Scissors", or else return an error message. 
+Define winning logic:
+    If computerChoice === 1 and humanChoice === 2, humanScore +1
+    If computerChoice === 1 and humanChoice === 3, computerScore +1
+    If computerChoice === 2 and humanChoice === 1, computerScore +1
+    If computerChoice === 2 and humanChoice === 3, humanScore +1
+    If computerChoice === 3 and humanChoice === 1, humanScore +1
+    If computerChoice === 3 and humanChoice === 2, computerScore +1
+    Else if computerChoice === humanChoice, return nothing
+Print humanChoice and computerChoice.
+Print message declaring the winner.
+Print humanScore and computerScore.
+*/
+
+//Returns a value between 1 (inclusive) and 4 (exclusive) at random.
+
+let humanScore = 0;
+let computerScore = 0;
+
+let computerChoice = getComputerChoice();
+
+function getComputerChoice() {
+    const minCeiled = Math.ceil(1);
+    const maxFloored = Math.floor(4);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("Choose your weapon! Rock, Paper or Scissors?");
+    if(humanChoice === computerChoice) {
+        // does nothing
+    } else if((computerChoice === 1 && humanChoice === 2) 
+        || (computerChoice === 2 && humanChoice === 3)
+        || (computerChoice === 3 && humanChoice === 1)) {
+            humanScore += 1;
+    } else if((computerChoice === 1 && humanChoice === 3)
+        || (computerChoice === 2 && humanChoice === 1)
+        || (computerChoice === 3 && humanChoice === 2)) {
+            computerScore += 1;}
+    }
