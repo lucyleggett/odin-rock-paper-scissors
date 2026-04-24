82dbf7471906578b95cb8670d2afdf9df9a728cf
lucyleggett <lucy.leggett@hotmail.com>
2026-04-24 12:47:52 +0800

    Add roundCount counter variable, trigger results after five rounds and move computerChoice declaration to within eventListeners roundCount added to eventListeners to increment with each round played. If clause added to playRound so that after 5 rounds have been played, the game is over and the results will be shown to the player. computerChoice declaration moved to inside the eventListeners so that a new choice is made with every round played.

--- rps.js
+++ rps.js
@@ -20,24 +20,31 @@ let humanScore = 0;
 let computerScore = 0;
 
 let humanChoice;
-let computerChoice = getComputerChoice();
+
+let roundCount = 0;
 
 const rockButton = document.querySelector("#rock");
 const paperButton = document.querySelector("#paper");
 const scissorsButton = document.querySelector("#scissors");
 
 rockButton.addEventListener("click",(e) => {
+    let computerChoice = getComputerChoice();
     humanChoice = "Rock";
+    roundCount += 1;
     playRound(humanChoice, computerChoice);
 })
 
 paperButton.addEventListener("click",(e) => {
+    let computerChoice = getComputerChoice();
     humanChoice = "Paper";
+    roundCount += 1;
     playRound(humanChoice, computerChoice);
 })
 
 scissorsButton.addEventListener("click",(e) => {
+    let computerChoice = getComputerChoice();
     humanChoice = "Scissors";
+    roundCount += 1;
     playRound(humanChoice, computerChoice);
 })
 
@@ -58,29 +65,31 @@ function getComputerChoice() {
 }
 
 function playRound(humanChoice,computerChoice) {
-    const roundResult = document.createElement("div");
-    if(humanChoice === computerChoice){
-        roundResult.textContent = `The computer played ${computerChoice} and you played ${humanChoice}. It's a tie...`;
-    }
-    else if((computerChoice === "Rock" && humanChoice === "Paper")
-    || (computerChoice === "Paper" && humanChoice === "Scissors")
-    || (computerChoice === "Scissors" && humanChoice === "Rock")){
-        humanScore += 1;
-        roundResult.textContent = `The computer played ${computerChoice} and you played ${humanChoice}. You win!`
+    const roundResult = document.querySelector("#results");
+    if (roundCount < 5) {
+        if(humanChoice === computerChoice){
+            roundResult.textContent = `The computer played ${computerChoice} and you played ${humanChoice}. It's a tie...`;
         }
-    else if((computerChoice === "Rock" && humanChoice === "Scissors")
-    || (computerChoice === "Paper" && humanChoice === "Rock")
-    || (computerChoice === "Scissors" && humanChoice === "Paper")){
-        computerScore += 1;
-        roundResult.textContent = `The computer played ${computerChoice} and you played ${humanChoice}.`
+        else if((computerChoice === "Rock" && humanChoice === "Paper")
+        || (computerChoice === "Paper" && humanChoice === "Scissors")
+        || (computerChoice === "Scissors" && humanChoice === "Rock")){
+            humanScore += 1;
+            roundResult.textContent = `The computer played ${computerChoice} and you played ${humanChoice}. You win!`
+            }
+        else if((computerChoice === "Rock" && humanChoice === "Scissors")
+        || (computerChoice === "Paper" && humanChoice === "Rock")
+        || (computerChoice === "Scissors" && humanChoice === "Paper")){
+            computerScore += 1;
+            roundResult.textContent = `The computer played ${computerChoice} and you played ${humanChoice}.`
+        }
+        const scores = document.createElement("p");
+        scores.textContent = `Your score: ${humanScore}\nComputer score: ${computerScore}`;
+        roundResult.appendChild(scores);
+        getComputerChoice();
+    } 
+    else {
+        const finalScores = document.createElement("h2");
+        finalScores.textContent = `Five rounds well played!\nFinal scores are ${humanScore} to you and ${computerScore} to the computer.`;
+        scores.appendChild(finalScores);
     }
-    const scores = document.createElement("p");
-    scores.textContent = `Your score: ${humanScore}\nComputer score: ${computerScore}`;
-    roundResult.appendChild(scores);
-    getComputerChoice();
 }
-
-    //Announces the winner
-    const finalScores = document.createElement("h2");
-    finalScores.textContent = `Final scores are ${humanScore} to you and ${computerScore} to the computer. ${victoryMessage}`;
-    scores.appendChild(finalScores);
