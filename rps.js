9ec47833361f0fa9666a4c0878104cea2a1c4784
lucyleggett <lucy.leggett@hotmail.com>
2026-04-24 17:18:01 +0800

    Create functions for each human play option and name them in the eventListeners removeEventListener can't be used on anonymous functions, which was blocking me from being able to stop the endGame function from running repeatedly from round 5 onwards.

--- rps.js
+++ rps.js
@@ -18,35 +18,38 @@ Print humanScore and computerScore.
 
 let humanScore = 0;
 let computerScore = 0;
+let roundCount = 0;
 
 let humanChoice;
-
-let roundCount = 0;
 
 const rockButton = document.querySelector("#rock");
 const paperButton = document.querySelector("#paper");
 const scissorsButton = document.querySelector("#scissors");
 
-rockButton.addEventListener("click",(e) => {
-    let computerChoice = getComputerChoice();
+function playRock() {
     humanChoice = "Rock";
-    roundCount += 1;
-    playRound(humanChoice, computerChoice);
-})
-
-paperButton.addEventListener("click",(e) => {
     let computerChoice = getComputerChoice();
+    roundCount += 1;
+    roundCount < 5 ? playRound(humanChoice, computerChoice): endGame();
+}
+
+function playPaper() {
     humanChoice = "Paper";
-    roundCount += 1;
-    playRound(humanChoice, computerChoice);
-})
-
-scissorsButton.addEventListener("click",(e) => {
     let computerChoice = getComputerChoice();
-    humanChoice = "Scissors";
     roundCount += 1;
-    playRound(humanChoice, computerChoice);
-})
+    roundCount < 5 ? playRound(humanChoice, computerChoice): endGame();
+}
+
+function playScissors() {
+    humanChoice = "Scissors";
+    let computerChoice = getComputerChoice();
+    roundCount += 1;
+    roundCount < 5 ? playRound(humanChoice, computerChoice): endGame();
+}
+
+rockButton.addEventListener("click", playRock);
+paperButton.addEventListener("click", playPaper);
+scissorsButton.addEventListener("click", playScissors);
 
 //Returns a value between 1 (inclusive) and 4 (exclusive) at random.
 function getComputerChoice() {
@@ -67,32 +70,39 @@ function getComputerChoice() {
 function playRound(humanChoice,computerChoice) {
     const roundResult = document.querySelector("#results");
     roundResult.style.whiteSpace = "pre-line";
-    if (roundCount < 5) {
-        if(humanChoice === computerChoice){
-            roundResult.textContent = `The computer played ${computerChoice} and you played ${humanChoice}. It's a tie...`;
-        }
-        else if((computerChoice === "Rock" && humanChoice === "Paper")
-        || (computerChoice === "Paper" && humanChoice === "Scissors")
-        || (computerChoice === "Scissors" && humanChoice === "Rock")){
-            humanScore += 1;
-            roundResult.textContent = `The computer played ${computerChoice} and you played ${humanChoice}. You win!`
-            }
-        else if((computerChoice === "Rock" && humanChoice === "Scissors")
-        || (computerChoice === "Paper" && humanChoice === "Rock")
-        || (computerChoice === "Scissors" && humanChoice === "Paper")){
-            computerScore += 1;
-            roundResult.textContent = `The computer played ${computerChoice} and you played ${humanChoice}. Tough luck, the computer wins!`
-        }
-        const scores = document.createElement("p");
-        scores.style.whiteSpace = "pre-line";
-        scores.textContent = `Your score: ${humanScore}\nComputer score: ${computerScore}`;
-        roundResult.appendChild(scores);
-        getComputerChoice();
-    } 
-    else {
-        const finalScores = document.createElement("h2");
-        finalScores.style.whiteSpace = "pre-line";
-        finalScores.textContent = `Five rounds well played!\nFinal scores are ${humanScore} to you and ${computerScore} to the computer.`;
-        roundResult.appendChild(finalScores);
+    if(humanChoice === computerChoice){
+        roundResult.textContent = `The computer played ${computerChoice} and you played ${humanChoice}. It's a tie...`;
     }
+    else if((computerChoice === "Rock" && humanChoice === "Paper")
+    || (computerChoice === "Paper" && humanChoice === "Scissors")
+    || (computerChoice === "Scissors" && humanChoice === "Rock")){
+        humanScore += 1;
+        roundResult.textContent = `The computer played ${computerChoice} and you played ${humanChoice}. You win!`
+        }
+    else if((computerChoice === "Rock" && humanChoice === "Scissors")
+    || (computerChoice === "Paper" && humanChoice === "Rock")
+    || (computerChoice === "Scissors" && humanChoice === "Paper")){
+        computerScore += 1;
+        roundResult.textContent = `The computer played ${computerChoice} and you played ${humanChoice}. Tough luck, the computer wins!`
+    }
+    const scores = document.createElement("p");
+    scores.style.whiteSpace = "pre-line";
+    scores.textContent = `Your score: ${humanScore}\nComputer score: ${computerScore}`;
+    roundResult.appendChild(scores);
+    getComputerChoice();
+}
+
+function endGame() {
+    const finalScores = document.createElement("h2");
+    finalScores.style.whiteSpace = "pre-line";
+    if (humanScore > computerScore) {
+        finalScores.textContent = `You're a winner! Maybe man will keep his dominion over machine for a couple more decades, after all.`;
+    }
+    else if (computerScore < humanScore) {
+        finalScores.textContent = `The computer said to tell you you're a dumb dumb loser...`;
+    }
+    results.appendChild(finalScores);
+    rockButton.removeEventListener("click", playRock);
+    paperButton.removeEventListener("click", playPaper);
+    scissorsButton.removeEventListener("click", playScissors);
 }
